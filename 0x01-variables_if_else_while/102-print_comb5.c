69a9a7fd423497c6c83b730ba09dc7d463969bc5
Odunayo Mayowa <67592259+linusvanpelt1@users.noreply.github.com>
2022-09-18 04:55:51 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,37 +6,33 @@
  */
 int main(void)
 {
-		int a, b, c, d;
+	int a, b, c, d;
 
-		for (a = '0'; a <= '9'; a++)
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
 		{
-			for (b = '0'; b <= '9'; b++)
+			for (c = '0'; c <= '9'; c++)
 			{
-				for (c = '0'; c <= '9'; c++)
+				for (d = '0'; d <= '9'; d++)
 				{
-					for (d = '0'; d <= '9'; d++)
+					if ((a < c) || (a == c && b < d))
 					{
-						if ((a < c) || (a == c && b < d))
-						{
-							putchar(a);
-							putchar(b);
-							putchar(' ');
-							putchar(c);
-							putchar(d);
-
-						if (!(a == '9' && b == '8'))
-						{
-							putchar(',');
-							putchar(' ');
-
-						}
-
-						}
+						putchar(a);
+						putchar(b);
+						putchar(' ');
+						putchar(c);
+						putchar(d);
+					if (!(a == '9' && b == '8'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
 					}
 				}
 			}
 		}
-		putchar('\n');
-		return (0);
-
+	}
+	putchar('\n');
+	return (0);
 }
