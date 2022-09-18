fe211138b8452b9d5af7c474636dc4e2cd608218
linusvanpelt1 <dmokag@gmail.com>
2022-09-17 17:45:11 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,31 +1,25 @@
 #include <stdio.h>
 
 /**
- *
- *  *main - entry point
- *
+ *  * main - print the string in the printf function
  *   *
- *
- *    *Return: Always zero (success)
- *
- *     */
-
+ *    * Description: using the main function
+ *     * this program prints "with proper grammar, but the outcome is a piece of art,
+ *      * Return: 0
+ *       */
 int main(void)
-
 {
+		char c;
+			int i;
+				long int li;
+					long long int lli;
+						float f;
 
-
-
-		printf("Size of a char: %d byte(s)\n", sizeof(char));
-
-			printf("Size of an int: %d byte(s)\n", sizeof(int));
-
-				printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-
-					printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-
-						printf("Size of a float: %d byte(s)\n", sizeof(float));
-
-							return (0);
-
+							printf("Size of a char: %ld byte(s)\n", (unsigned long)sizeof(c));
+								printf("Size of an int: %ld byte(s)\n", (unsigned long)sizeof(i));
+									printf("Size of a long int: %ld byte(s)\n",  (unsigned long)sizeof(li));
+										printf("Size of a long long int: %ld byte(s)\n",  (unsigned long)sizeof(lli));
+											printf("Size of a float: %ld byte(s)\n", (unsigned long)sizeof(f));
+												return (0);
 }
+
