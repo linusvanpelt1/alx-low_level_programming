19ec2e24e688caa5fe36e0a1b364e2c2b8bd37a3
linusvanpelt1 <dmokag@gmail.com>
2022-09-24 10:17:28 -0700

    nested looping

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,41 @@
 #include "main.h"
+
+
+
 /**
- * main - Prints Holberton
  *
- * Return: Always 0 (Success)
- */
+ *  * main - print _putchar
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+
+
 int main(void)
+
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+
+		_putchar('_');
+
+			_putchar('p');
+
+				_putchar('u');
+
+					_putchar('t');
+
+						_putchar('c');
+
+							_putchar('h');
+
+								_putchar('a');
+
+									_putchar('r');
+
+										_putchar('\n');
+
+											return (0);
+
 }
