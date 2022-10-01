e604650db5742815ad902098ce2ce6ce1a62b22c
linusvanpelt1 <mayowakodunayo@gmail.com>
2022-10-01 23:36:44 +0100

    all updates

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,12 +1,6 @@
-#ifndef  MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
-/*
- * File: main.h
- * Auth: MAYOWA
- * Desc: Header file containing the prototypes of all functions
- *       included in the static library main.a.
- */
 
 int _putchar(char c);
 int _islower(int c);
@@ -29,4 +23,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* MAIN_H */
