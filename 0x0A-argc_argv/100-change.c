0e5e41e1cd85ceb589953b45e791a8cc8c3aa107
Odunayo Mayowa <67592259+linusvanpelt1@users.noreply.github.com>
2022-10-01 17:48:05 +0100

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,72 +1,54 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <stdbool.h>
 
 /**
- * coinConverter - Helper function that does all the mathematics
- * @i: Passed in variable from main for calculations
- * Return: The number of coins needed minimum for the passed in variable
+ * isInt - Checkes if s is an integer
+ * @s: string to validate
+ * Return: Always 0. (Success)
+ * otherwise: Always 1.
  */
-int coinConverter(int i)
+int isInt(char *s)
 {
-	int count = 0;
-
-	while (i != 0)
+	int i = 0;
+	
+	for(i = 0; s[i] != '\0'; i++)
 	{
-		if (i % 10 == 9 || i % 10 == 7)
-			i -= 2;
-		else if (i % 25 == 0)
-			i -= 25;
-		else if (i % 10 == 0)
-			i -= 10;
-		else if (i % 5 == 0)
-			i -= 5;
-		else if (i % 2 == 0)
-		{
-			if (i % 10 == 6)
-				i -= 1;
-			else
-				i -= 2;
-		}
-		else
-			i -= 1;
-
-		count++;
+		if(s[i] < '0' || s[i] > '9')
+			return (0);
 	}
-
-	return (count);
+	return (1);
 }
 
 /**
- * main - Takes in exactly one argument for minimum coin count
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
+ * main - prints the minimum number of coins to make change for an amount of money
+ * @argc: arguement count
+ * @argv: arguement vector.
+ * Return: Always 0.(Success)
  */
 int main(int argc, char *argv[])
 {
-	int i, coin;
+	int i = 0, coinUsed = 0, coin = 0;
+	int coins[] = {25, 10, 5, 2, 1};
 
-	coin = 0;
-
-	if (argc != 2)
+	if(argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	i = atoi(argv[1]);
-
-	if (i < 0)
-		printf("0\n");
-	else
+	if(isInt(argv[1]))
 	{
-		coin = coinConverter(i);
-
-		printf("%d\n", coin);
+		i = atoi(argv[1]);
+		while(i > 0 && coin <=4)
+		{
+			if(i >= coins[coin])
+			{
+				i -= coins[coin];
+				coinUsed++;
+			}
+			else
+				coin++;
+		}
 	}
-
+	printf("%i\n", coinUsed);
 	return (0);
 }
