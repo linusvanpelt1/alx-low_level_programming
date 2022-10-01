0eb1b56bf8a9b13997bd9cb52407fa61a10a8aa7
Odunayo Mayowa <67592259+linusvanpelt1@users.noreply.github.com>
2022-10-01 17:51:01 +0100

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,53 +3,42 @@
 #include "main.h"
 
 /**
- * isInt - Checkes if s is an integer
- * @s: string to validate
- * Return: Always 0. (Success)
- * otherwise: Always 1.
- */
-int isInt(char *s)
-{
-	int i = 0;
-	
-	for(i = 0; s[i] != '\0'; i++)
-	{
-		if(s[i] < '0' || s[i] > '9')
-			return (0);
-	}
-	return (1);
-}
-
-/**
- * main - prints the minimum number of coins to make change for an amount of money
- * @argc: arguement count
- * @argv: arguement vector.
- * Return: Always 0.(Success)
+ * main - prints the minimum number of coins to
+ * make change for an amount of money
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
 {
-	int i = 0, coinUsed = 0, coin = 0;
+	int num, j, result;
 	int coins[] = {25, 10, 5, 2, 1};
 
-	if(argc != 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if(isInt(argv[1]))
+
+	num = atoi(argv[1]);
+	result = 0;
+
+	if (num < 0)
 	{
-		i = atoi(argv[1]);
-		while(i > 0 && coin <=4)
+		printf("0\n");
+		return (0);
+	}
+
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
 		{
-			if(i >= coins[coin])
-			{
-				i -= coins[coin];
-				coinUsed++;
-			}
-			else
-				coin++;
+			result++;
+			num -= coins[j];
 		}
 	}
-	printf("%i\n", coinUsed);
+
+	printf("%d\n", result);
 	return (0);
 }
